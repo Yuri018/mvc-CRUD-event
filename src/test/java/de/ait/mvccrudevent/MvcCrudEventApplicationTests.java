dc85ab92e780c77ed33df5e86ee269091fb207f6
Juri Ryzhkov <plktmn@gmail.com>
2024-01-24 20:10:59 +0100

    Initial commit

---
+++ src/test/java/de/ait/mvccrudevent/MvcCrudEventApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ait.mvccrudevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCrudEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
